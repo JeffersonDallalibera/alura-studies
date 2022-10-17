916ddea2999a7167684f9e073b9d501fdc1b0638
JEFFERSON MIGUEL DALLALIBERA <jeffersonmigueldallalibera@gmail.com>
2022-10-17 10:54:32 -0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import teste from './teste.jpeg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={teste} className="teste" alt="Amo você"/>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+           <code>Eu te amo minha princesa</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>
+          Voce é tudo pra mim
         </a>
       </header>
     </div>
